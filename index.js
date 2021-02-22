553a9847122d4e0a3374424e1d336aaaefffad01
Adrian <adriangonz457@gmail.com>
2021-02-22 14:38:42 -0500

    Added google's speech-to-text modules and custom-env module.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('custom-env').env('staging');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -11,4 +12,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+client.login(process.env.BOT_TOKEN);
